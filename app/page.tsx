0ada402e691c532c54b0bf015a7dca33de9f8b27
crimsonraine <wukimberley98@gmail.com>
2024-11-15 09:42:37 -0500

    packages installed after running, minor syntax change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import { React } from 'react';
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+// import App from './App.js';
 
 export default function Page() {
+    <html lang="en"></html>
     return <h1>Hello, Next.js!</h1>
   }
