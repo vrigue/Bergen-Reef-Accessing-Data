aa115f55c491040cb07c73fb4a803df97ecad006
crimsonraine <wukimberley98@gmail.com>
2024-11-18 09:18:52 -0500

    hydration error suppression

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+// import { hydrateRoot } from 'react-dom/client';
 // import App from './App.js';
 
 export default function Page() {
