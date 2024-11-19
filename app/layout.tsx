5a216e9ea58b2ab9f171727e30c3da617f2554ac
crimsonraine <wukimberley98@gmail.com>
2024-11-19 09:35:25 -0500

    tailwind CSS initial files and implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import './globals.css' // These styles apply to every route in the application
+ 
+export const metadata = {
+  title: 'Plumbing',
+  description: 'Due Thursday',
+}
 
 export default function RootLayout({
     children,
