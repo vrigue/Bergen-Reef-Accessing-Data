795fc1717251c918c111a9472cf56c56173393b1
anaajesh <anaghaajesh2010@gmail.com>
2025-01-09 00:46:49 -0500

    1) User list is made available for registered users and registered users only. Blocked from general user view
    2) profile page updates with user info once logged in

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,6 +10,11 @@ import ProfileClient from "../components/ProfileClient";
 
 
 export default function Page() {
+
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div>
       <div className="flex items-center justify-between bg-teal p-4 shadow-lg rounded-lg">
@@ -85,10 +90,19 @@ export default function Page() {
 
       {/* top orange box*/}
       <div className="bg-light-orange p-6 rounded-lg shadow-lg flex justify-between items-center w-1/2 mx-auto">
+        
+        {user ? (
+          <div>
+            <h1 className="text-2xl font-semibold">{user.name}</h1>
+            <p className="text-sm">{user.email}</p>
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-2xl font-semibold">General User</h1>
+            <p className="text-sm">Login for User Info!</p>
+          </div>
+        )}
         <div>
-          <h2 className="font-bold text-lg">Admin</h2>
-          <h1 className="text-2xl font-semibold">Dr. Reeves</h1>
-          <p className="text-sm">davree@gmail.com</p>
         </div>
         <div className="flex flex-col space-y-4">
           <a
@@ -106,76 +120,63 @@ export default function Page() {
         </div>
       </div>
       
-    <br></br>  
-    <div className="flex justify-center">
-      <div className="rounded-md justify-content-center w-2/3 bg-gray-100 p-4" style={{height:400}}>
+      <br></br>  
 
-          <div className="bg-white rounded-lg shadow-lg p-6" style={{height:370}}>
-            <h3 className="font-bold text-lg mb-4">Manage Users</h3>
-            <div className="flex items-center mb-4">
-              <span className="h-4 w-4 bg-black rounded-full inline-block mr-4"></span>
-              <p className="flex-1">Henry Ramirez</p>
-              <select className="border rounded px-2 py-1">
-                <option value="Admin">Admin</option>
-                <option value="General">General</option>
-              </select>
-            </div>
-            <div className="flex items-center mb-4">
-              <span className="h-4 w-4 bg-black rounded-full inline-block mr-4"></span>
-              <p className="flex-1">Ben Isecke</p>
-              <select className="border rounded px-2 py-1">
-                <option value="Admin">Admin</option>
-                <option value="General">General</option>
-              </select>
-            </div>
-            <div className="flex items-center mb-4">
-              <span className="h-4 w-4 bg-black rounded-full inline-block mr-4"></span>
-              <p className="flex-1">Vrimagha Guejesni</p>
-              <select className="border rounded px-2 py-1">
-                <option value="Admin">Admin</option>
-                <option value="General">General</option>
-              </select>
-            </div>
-            
-            <br></br>
-            <br></br>
+      {user && (
+        <div className="flex justify-center">
+        <div className="rounded-md justify-content-center w-2/3 bg-gray-100 p-4" style={{height:400}}>
 
-            <div className="flex justify-center space-x-4">
-              <a
-                href="/api/auth/login"
-                className="bg-black text-white px-6 py-2 rounded-md shadow-lg hover:bg-orange-600 transition text-center"
-              >
-                Navigate to CSV Files
-              </a>
-              <a
-                href="/api/auth/login"
-                className="bg-black text-white px-6 py-2 rounded-md shadow-lg hover:bg-orange-600 transition text-center"
-              >
-                Sync Fusion Files
-              </a>
-            </div>
+            <div className="bg-white rounded-lg shadow-lg p-6" style={{height:370}}>
+              <h3 className="font-bold text-lg mb-4">Manage Users</h3>
+              <div className="flex items-center mb-4">
+                <span className="h-4 w-4 bg-black rounded-full inline-block mr-4"></span>
+                <p className="flex-1">Henry Ramirez</p>
+                <select className="border rounded px-2 py-1">
+                  <option value="Admin">Admin</option>
+                  <option value="General">General</option>
+                </select>
+              </div>
+              <div className="flex items-center mb-4">
+                <span className="h-4 w-4 bg-black rounded-full inline-block mr-4"></span>
+                <p className="flex-1">Ben Isecke</p>
+                <select className="border rounded px-2 py-1">
+                  <option value="Admin">Admin</option>
+                  <option value="General">General</option>
+                </select>
+              </div>
+              <div className="flex items-center mb-4">
+                <span className="h-4 w-4 bg-black rounded-full inline-block mr-4"></span>
+                <p className="flex-1">Vrimagha Guejesni</p>
+                <select className="border rounded px-2 py-1">
+                  <option value="Admin">Admin</option>
+                  <option value="General">General</option>
+                </select>
+              </div>
+              
+              <br></br>
+              <br></br>
 
-          </div>
-        
+              <div className="flex justify-center space-x-4">
+                <a
+                  href="/api/auth/login"
+                  className="bg-black text-white px-6 py-2 rounded-md shadow-lg hover:bg-orange-600 transition text-center"
+                >
+                  Navigate to CSV Files
+                </a>
+                <a
+                  href="/api/auth/login"
+                  className="bg-black text-white px-6 py-2 rounded-md shadow-lg hover:bg-orange-600 transition text-center"
+                >
+                  Sync Fusion Files
+                </a>
+              </div>
+
+            </div>
+        </div>
       </div>
-    </div>
+      )}
 
-      {/*<br></br>
-      <div className="flex justify-center space-x-4">
-        <a
-          href="/api/auth/login"
-          className="bg-orange text-white px-6 py-2 rounded-full shadow-lg hover:bg-orange-600 transition"
-        >
-          Login
-        </a>
 
-        <a
-          href="/api/auth/logout"
-          className="bg-orange text-white px-6 py-2 rounded-full shadow-lg hover:bg-orange-600 transition"
-        >
-          Logout
-        </a>
-      </div> */}
       <br></br>
       <br></br>
       <ProfileClient />
