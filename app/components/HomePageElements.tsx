2444cbfc440bed3113eb693ccd8b801c3ad309dd
vrigue <vrielleguevarra@gmail.com>
2025-04-28 10:34:06 -0400

    attempt to make home page adjust to various screen sizes

--- app/components/HomePageElements.tsx
+++ app/components/HomePageElements.tsx
@@ -51,72 +51,72 @@ export default function HomePageElements() {
             {/*PH*/}
             <div className="relative text-center">
             <img src="/images/coral-panel-1.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">PH</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[0]}</div>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">PH</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[0]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">pH</div>
             </div>
 
             {/*SALINITY*/}
             <div className="relative text-center">
             <img src="/images/basic-panel.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">SALINITY</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[1]}</div>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">SALINITY</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[1]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">ppt</div>
             </div>
 
             {/*TEMPERATURE*/}
             <div className="relative text-center">
-            <img src="/images/basic-panel.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">TEMPERATURE</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[2]}</div>
+            <img src="/images/coral-panel-2.png" style={{width: "100%", height: "auto"}}></img>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">TEMPERATURE</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[2]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">ºF</div>
             </div>
 
             {/*ORP*/}
             <div className="relative text-center">
-            <img src="/images/coral-panel-2.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">ORP</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[3]}</div>
+            <img src="/images/basic-panel.png" style={{width: "100%", height: "auto"}}></img>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">ORP</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[3]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">mV</div>
             </div>
 
             {/*ALKALINITY*/}
             <div className="relative text-center">
             <img src="/images/coral-panel-3.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">ALKALINITY</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[4]}</div>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">ALKALINITY</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[4]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">dkH</div>
             </div>
 
             {/*CALCIUM*/}
             <div className="relative text-center">
             <img src="/images/basic-panel.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">CALCIUM</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[5]}</div>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">CALCIUM</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[5]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">ppm</div>
             </div>
 
             {/*NITRATE*/}
             <div className="relative text-center">
-            <img src="/images/coral-panel-2.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">NITRATE</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[6]}</div>
+            <img src="/images/coral-panel-1.png" style={{width: "100%", height: "auto"}}></img>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">NITRATE</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[6]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">ppm</div>
             </div>
 
             {/*NITRITE*/}
             <div className="relative text-center">
-            <img src="/images/coral-panel-3.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">NITRITE</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[7]}</div>
+            <img src="/images/basic-panel.png" style={{width: "100%", height: "auto"}}></img>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">NITRITE</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[7]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">ppm</div>
             </div>
 
             {/*PHOSPHATE*/}
             <div className="relative text-center">
-            <img src="/images/basic-panel.png" style={{width: "100%", height: "auto"}}></img>
-            <div className="w-full absolute top-0 left-0 text-center text-white font-semibold mt-3 text-base">PHOSPHATE</div>
-            <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-5 text-5xl">{elementData[8]}</div>
+            <img src="/images/coral-panel-2.png" style={{width: "100%", height: "auto"}}></img>
+            <div className="w-full absolute top-0 left-0 text-center text-white text-sm font-semibold mt-3">PHOSPHATE</div>
+            <div className="w-full absolute top-10 left-0 text-center text-dark-gray text-5xl font-semibold mt-5">{elementData[8]}</div>
             <div className="w-full absolute top-10 left-0 text-center text-dark-gray font-semibold mt-14 pt-8 text-3xl">ppm</div>
             </div>
         </div>
