a16e37f9452cd0a288c5dc092c086a784ef147c7
crimsonraine <wukimberley98@gmail.com>
2025-05-14 23:44:04 -0400

    spreadsheet embed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -40,56 +40,82 @@ export default function Page() {
       <br></br>
 
       {/* top white box*/}
-      <div className="bg-white rounded shadow-sm p-6 rounded-lg flex justify-between items-center w-1/2 mx-auto">
-        
-        {user ? (
-          <div>
-            <h1 className="text-2xl font-semibold">{user.name}</h1>
-            <h3 className="text-sm">{user.email}</h3>
-          </div>
-        ) : (
-          <div>
-            <h1 className="text-2xl font-semibold">General User</h1>
-            <p className="text-sm">Login for User Info!</p>
-          </div>
-        )}
-        <div>
-        </div>
-        <div className="flex flex-col space-y-4">
-          {!user && (
-            <a
-            href="/api/auth/login"
-            className="bg-teal text-white px-6 py-2 rounded-xl shadow-lg hover:bg-orange-600 transition text-center"
-          >
-             Login
-          </a>
+      <div className="bg-white rounded shadow-sm p-6 rounded-lg flex justify-between items-center w-3/4 mx-auto">
+        <div className="flex items-center gap-4">
+          {user ? (
+            <>
+              <div>
+                <h1 className="text-2xl font-semibold">{user.name}</h1>
+                <h3 className="text-sm">{user.email}</h3>
+              </div>
+              <a
+                href="/api/auth/logout"
+                className="bg-teal text-white px-6 py-2 rounded-xl shadow-lg hover:bg-medium-teal transition"
+                style={{
+                  padding: "8px 16px",
+                  fontSize: "16px",
+                  color: "white",
+                  border: "none",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <div>
+                <h1 className="text-2xl font-semibold">General User</h1>
+                <p className="text-sm">Login for User Info!</p>
+              </div>
+              <a
+                href="/api/auth/login"
+                className="bg-teal text-white px-6 py-2 rounded-xl shadow-lg hover:bg-orange-600 transition text-center"
+                style={{
+              padding: "8px 16px",
+              fontSize: "16px",
+              color: "white",
+              border: "none",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              }}
+              >
+                Login
+              </a>
+            </>
           )}
-          <a
-            href="/api/auth/logout"
-            className="bg-teal text-white px-6 py-2 rounded-xl shadow-lg hover:bg-orange-600 transition"
-          >
-            Logout
-          </a>
         </div>
+
+        <a
+          target="_blank"
+          href="https://docs.google.com/spreadsheets/d/1BF5JBYV3v2brBQQaRBo6X_J-uPCdDtzE4UUCDKtLPJA/edit?usp=sharing"
+          className="bg-teal text-white px-6 py-2 rounded-xl shadow-lg hover:bg-medium-teal transition"
+        >
+          View Data Backup Spreadsheet in New Tab <ArrowUpRightIcon className="inline w-4 h-4 text-white ml-1 align-text-bottom"/>
+        </a>
       </div>
       
       <br></br>  
 
       {isAdmin && (
         <div className="flex justify-center">
-          <div className="rounded-md justify-content-center w-2/3 bg-gray-100 p-4" style={{height:400}}>
+          <div className="rounded-md justify-content-center w-1/2 bg-gray-100 p-4" style={{height:400}}>
             <ResponsiveContainer>
               <UserList/>
             </ResponsiveContainer>
           </div>
 
-          <a
-            target="_blank"
-            href="https://docs.google.com/spreadsheets/d/1BF5JBYV3v2brBQQaRBo6X_J-uPCdDtzE4UUCDKtLPJA/edit?usp=sharing"
-            className="bg-teal text-white px-6 py-2 mt-5 rounded-xl shadow-lg hover:bg-medium-teal transition"
-          >
-            View Data Backup Spreadsheet <ArrowUpRightIcon className="inline w-4 h-4 text-white ml-1 align-text-bottom"/>
-          </a>
+          <div className="ml-4">
+            <iframe 
+              src="https://docs.google.com/spreadsheets/d/1BF5JBYV3v2brBQQaRBo6X_J-uPCdDtzE4UUCDKtLPJA/edit?usp=sharing&embedded=true"
+              style={{width: '600px', height: '400px', outline: 'none'}}
+              frameBorder="0"
+              className="rounded-xl shadow-lg"
+            />
+          </div>
         </div>
       )}
     </div>
